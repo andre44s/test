2ce2ebab01a4e4a93b09b1e9f9ba4e45cbaae385
Yohanes Andre Setiawan <yohanes.setiawan@colearn.id>
2022-11-28 19:14:39 +0800

    asdf

--- main.py
+++ main.py
@@ -449,9 +449,11 @@ with tab2: # === MSD Data File Processing ===
                     reader = utils.get_reader()
                     start_time = datetime.datetime.now()
                     img_files = asyncio.run(utils.get_img(files, batch_count))  # this is for running locally
+                    print(1)
                     result = Parallel(n_jobs=2, prefer='threads', verbose=10)(delayed(utils.extract_text)(i, reader) for i in img_files[0:])
                     st.text(datetime.datetime.now() - start_time)
                     logger.info(datetime.datetime.now() - start_time)
+                    print(1)
 
                     text_df['bounding_box'] = [i[0] for i in result]
                     text_df['extracted_text'] = [i[1] for i in result]
