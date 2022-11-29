1b6e4115721acfea0a1cfe80e90ba95e1d5cca81
Yohanes Andre Setiawan <yohanes.setiawan@colearn.id>
2022-11-29 12:03:13 +0800

    asdfasdfsadf

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from easyocr import Reader
 import requests
+import pandas as pd
 
 input_csv = st.file_uploader("Choose File", type="csv", accept_multiple_files=False, key=None, help=None)
 
 if input_csv != None:
     reader = Reader(['id'], gpu=False)
-    image_link = input_csv["imageUrl"].values
+    image_link = input_csv[["imageUrl"]].values
     o = 0
     for i in image_link:
         response = requests.get(i)
